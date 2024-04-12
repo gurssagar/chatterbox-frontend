3004813960cce41e9966ad16ea98f9b15933afa7
Harjot Singh <HarjjotSinghh@gmail.com>
2024-04-12 10:14:29 +0530

    add: landing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
 import "@/styles/globals.css";
-
 import { SocketProvider } from "@/context/socket";
+import { Montserrat } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "next-themes";
+
+const monserrat = Montserrat({
+  subsets: ["latin"],
+  weight: "variable",
+  display: "swap"
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <SocketProvider>
-      <Component {...pageProps} />
-    </SocketProvider>
+    <main className={cn(monserrat.className, "light")}>
+      <ThemeProvider enableSystem={true} themes={["light", "dark"]} defaultTheme="dark" attribute="class">
+        <SocketProvider>
+          <Component {...pageProps} />
+        </SocketProvider>
+      </ThemeProvider>
+    </main>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,60 @@
-import { v4 as uuidv4 } from 'uuid';
-import { useRouter } from 'next/navigation'
-
-import styles from '@/styles/home.module.css'
-import { useState } from 'react';
+import { v4 as uuidv4 } from "uuid";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Image from "next/image";
+import { IoIosAddCircle } from "react-icons/io";
+import { Input } from "@/components/ui/input";
+import { HeroHighlight, Highlight } from "@/components/ui/hero-highlight";
+import { motion } from "framer-motion";
 
 export default function Home() {
-  const router = useRouter()
-  const [roomId, setRoomId] = useState('')
+  const router = useRouter();
+  const [roomId, setRoomId] = useState("");
 
   const createAndJoin = () => {
-    const roomId = uuidv4()
-    router.push(`/${roomId}`)
-  }
+    const roomId = uuidv4();
+    router.push(`/${roomId}`);
+  };
 
   const joinRoom = () => {
-    if (roomId) router.push(`/${roomId}`)
+    if (roomId) router.push(`/${roomId}`);
     else {
-      alert("Please provide a valid room id")
+      alert("Please provide a valid room id");
     }
-  }
+  };
   return (
-    <div className={styles.homeContainer}>
-        <h1>Google Meet Clone</h1>
-        <div className={styles.enterRoom}>
-          <input placeholder='Enter Room ID' value={roomId} onChange={(e) => setRoomId(e?.target?.value)}/>
-          <button onClick={joinRoom}>Join Room</button>
-        </div>
-        <span  className={styles.separatorText} >--------------- OR ---------------</span>
-        <button onClick={createAndJoin}>Create a new room</button>
+    <div className="min-h-screen gap-8 flex flex-col justify-center items-center px-4">
+      
+      {/* <h1 className="text-5xl font-extrabold tracking-tighter">Chatterbox</h1> */}
+      <Image
+        src={"/logos/svgexport-27.svg"}
+        width={1000}
+        height={1000}
+        className="sm:w-96 w-full h-auto"
+        alt="Chatterbox Logo"
+      ></Image>
+      <Input
+        className="sm:w-96 w-full"
+        placeholder="Enter Room ID"
+        value={roomId}
+        onChange={(e) => setRoomId(e?.target?.value)}
+      />
+      <div className="flex flex-row sm:w-96 w-full justify-center items-center flex-wrap gap-4">
+        <button
+          className="bg-primary/5 px-4 py-2 rounded-lg border-2 border-primary/50 hover:bg-primary/10 transition-all duration-300 ease-in-out hover:border-primary/80 inline-flex flex-row flex-wrap items-center gap-1"
+          onClick={joinRoom}
+        >
+          <IoIosAddCircle className="lg:size-6 size-5" />
+          <span className="sm:text-base text-sm font-semibold">Join Room</span>
+        </button>
+        <button
+          className="bg-primary/5 px-4 py-2 rounded-lg border-2 border-primary/50 hover:bg-primary/10 transition-all duration-300 ease-in-out hover:border-primary/80 inline-flex flex-row flex-wrap items-center gap-1"
+          onClick={joinRoom}
+        >
+          <IoIosAddCircle className="lg:size-6 size-5" />
+          <span className="sm:text-base text-sm font-semibold">Create Room</span>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
